1b7c580148d60ae886f94491fb2cd7b63a520d60
Pieterjan Van Saet <hybridfox2@gmail.com>
2017-12-28 00:14:10 +0100

    Externe DB

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'klimkaffee');
+define('DB_NAME', 'zadmin_klimkaffee');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'klimkaffee');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'na5ybyzed');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', '91.121.170.187');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
