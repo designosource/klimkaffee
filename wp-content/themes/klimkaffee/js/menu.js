5761e03a05208d23d4a75d22d91410c9a6f96cee
Lisa9W <lisa@m-release.com>
2018-01-25 11:19:29 +0100

    sub menus open up

--- wp-content/themes/klimkaffee/js/menu.js
+++ wp-content/themes/klimkaffee/js/menu.js
@@ -1,8 +1,13 @@
 $(document).ready(function () {
     $("nav.main").hide();
+    $("nav.main li.menu-item-has-children ul").hide();
 
     $(".hamburger").on("click", function () {
         $(this).toggleClass("open");
         $("nav.main").slideToggle("slow");
+
+        $("nav.main li.menu-item-has-children").on("click", function () {
+            $(this).children("ul").slideToggle("slow");
+        });
     });
 });
