ab08128a2a85c5bb7d08d6fa9abab345d5d89312
HybridFox <hybridfox2@gmail.com>
2018-03-12 17:47:29 +0100

    memes

--- wp-content/themes/klimkaffee/functions.php
+++ wp-content/themes/klimkaffee/functions.php
@@ -1,4 +1,8 @@
 <?php
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
+
 function wpb_custom_new_menu() {
 	register_nav_menus(
 		array(
--- wp-content/themes/klimkaffee/page-trainers.php
+++ wp-content/themes/klimkaffee/page-trainers.php
@@ -24,11 +24,9 @@ Template Name: Trainers
                 <div class="col-md-9">
                     <h2><?php the_title(); ?></h2>
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-12">
+                            <img class="col-md-12 inline-image" src="<?php echo get_field('trainer_afbeelding')['url'] ?>" alt="<?php echo get_field('trainer_afbeelding')['alt'] ?>">
                             <?php the_field( 'trainer_omschrijving' ) ?>
-                        </div>
-                        <div class="col-md-6">
-                            <img src="<?php echo get_field('trainer_afbeelding')['url'] ?>" alt="<?php echo get_field('trainer_afbeelding')['alt'] ?>">
                         </div>
                     </div>
                 </div>
