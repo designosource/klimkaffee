50b07010b5561bdc44f05cdbb6ee29a50189065b
Pieterjan Van Saet <hybridfox2@gmail.com>
2018-03-12 18:18:39 +0100

    Rename

--- wp-content/themes/klimkaffee/page-planner.php
+++ wp-content/themes/klimkaffee/page-planner.php
@@ -5,7 +5,7 @@ include( 'banner.php' ) ?>
             <div class="row">
                 <div class="col-md-9">
                     <h2><?php the_title(); ?></h2>
-                    <a href="/planner/registreer">REG</a>
+                    <a href="/planner/register">REG</a>
                     <a href="/planner/shop">SHOP</a>
                     <a href="/planner/reserveer">RESERVEER</a>
                 </div>
