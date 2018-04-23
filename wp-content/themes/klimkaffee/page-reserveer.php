7b157b3e0da3c2ef3749d3f320da8f4aba215530
Pieterjan Van Saet <hybridfox2@gmail.com>
2018-04-23 18:50:19 +0200

    Clubplanner edits

--- wp-content/themes/klimkaffee/page-reserveer.php
+++ wp-content/themes/klimkaffee/page-reserveer.php
@@ -1,10 +1,10 @@
 <?php get_header();?>
-<div class="page-title"
-     style="background-image: url( <?php echo $post->post_parent > 0 ?  wp_get_attachment_url(get_post_thumbnail_id($post->post_parent)) :  wp_get_attachment_url(get_post_thumbnail_id($post)); ?>)">
-    <h1>Shop</h1>
-</div>
     <div class="page-content page-shop">
         <div class="custom-container">
+            <div class="page-title"
+                 style="background-image: url( <?php echo $post->post_parent > 0 ?  wp_get_attachment_url(get_post_thumbnail_id($post->post_parent)) :  wp_get_attachment_url(get_post_thumbnail_id($post)); ?>)">
+                <h1>Reserveer</h1>
+            </div>
                     <?php
                     if ( have_posts() ) : while ( have_posts() ) : the_post();
                         get_template_part( 'content-only-content', get_post_format() );
