8039d8b16af1c52bf4ec559e61a5bf7399694505
Lisa9W <lisa@m-release.com>
2018-04-26 12:15:09 +0200

    test

--- wp-content/themes/klimkaffee/page-planner.php
+++ wp-content/themes/klimkaffee/page-planner.php
@@ -4,16 +4,19 @@ include( 'banner.php' ) ?>
         <div class="custom-container">
             <div class="row">
                 <div class="col-md-4 planner-link">
-                    <a href="<?php echo get_site_url(); ?>/planner/register" class="planner-link-image" style="background-image: url( <?php echo $post->post_parent > 0 ?  wp_get_attachment_url(get_post_thumbnail_id($post->post_parent)) :  wp_get_attachment_url(get_post_thumbnail_id($post)); ?>)"></a>
                     <a href="<?php echo get_site_url(); ?>/planner/register">REGISTREER</a>
+                    <a href="<?php echo get_site_url(); ?>/planner/register" class="planner-link-image" style="background-image: url(<?php the_field( 'registreer_foto' ); ?>)"></a>
+
                 </div>
                 <div class="col-md-4 planner-link">
-                    <a href="<?php echo get_site_url(); ?>/planner/shop" class="planner-link-image" style="background-image: url( <?php echo $post->post_parent > 0 ?  wp_get_attachment_url(get_post_thumbnail_id($post->post_parent)) :  wp_get_attachment_url(get_post_thumbnail_id($post)); ?>)"></a>
                     <a href="<?php echo get_site_url(); ?>/planner/shop">SHOP</a>
+                    <a href="<?php echo get_site_url(); ?>/planner/shop" class="planner-link-image" style="background-image: url<?php the_field( 'shop_foto' ); ?>?>)"></a>
+
                 </div>
                 <div class="col-md-4 planner-link">
-                    <a href="<?php echo get_site_url(); ?>/planner/reserveer"class="planner-link-image" style="background-image: url( <?php echo $post->post_parent > 0 ?  wp_get_attachment_url(get_post_thumbnail_id($post->post_parent)) :  wp_get_attachment_url(get_post_thumbnail_id($post)); ?>)"></a>
                     <a href="<?php echo get_site_url(); ?>/planner/reserveer">RESERVEER</a>
+                    <a href="<?php echo get_site_url(); ?>/planner/reserveer"class="planner-link-image" style="background-image: url(<?php the_field( 'reserveer_foto' ); ?>)"></a>
+
                 </div>
             </div>
         </div>
