ac0860279bd6b47911932db40c15d67d6325a1d0
Lisa9W <lisa@m-release.com>
2018-04-26 20:31:30 +0200

    Planner page fix

--- wp-content/themes/klimkaffee/page-planner.php
+++ wp-content/themes/klimkaffee/page-planner.php
@@ -6,17 +6,17 @@ include( 'banner.php' ) ?>
                 <div class="col-md-4 planner-link">
                     <a href="<?php echo get_site_url(); ?>/planner/register">REGISTREER</a>
                     <a href="<?php echo get_site_url(); ?>/planner/register" class="planner-link-image" style="background-image: url(<?php the_field( 'registreer_foto' ); ?>)"></a>
-
+                    <?php the_field( 'registreer_uitleg' ); ?>
                 </div>
                 <div class="col-md-4 planner-link">
                     <a href="<?php echo get_site_url(); ?>/planner/shop">SHOP</a>
                     <a href="<?php echo get_site_url(); ?>/planner/shop" class="planner-link-image" style="background-image: url<?php the_field( 'shop_foto' ); ?>?>)"></a>
-
+                    <?php the_field( 'shop_uitleg' ); ?>
                 </div>
                 <div class="col-md-4 planner-link">
                     <a href="<?php echo get_site_url(); ?>/planner/reserveer">RESERVEER</a>
                     <a href="<?php echo get_site_url(); ?>/planner/reserveer"class="planner-link-image" style="background-image: url(<?php the_field( 'reserveer_foto' ); ?>)"></a>
-
+                    <?php the_field( 'reserveer_uitleg' ); ?>
                 </div>
             </div>
         </div>
